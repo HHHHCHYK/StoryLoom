ea14938f05b19ef6a3eb0b0846737d67ec2c2b52
hykal <hchyk66@163.com>
2026-02-17 21:36:28 +0800

    Initialize Git

---
+++ StoryLoom/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace StoryLoom
+{
+    // 这里肯定被 Rider 错改成了 public partial class MainUI : Application
+    // 请把它改回 App！
+    public partial class App : Application
+    {
+        
+    }
+}
---
+++ StoryLoom/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StoryLoom;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddBlazorWebView();
+        serviceCollection.AddBlazorWebViewDeveloperTools();
+        Resources.Add("StoryLoom", serviceCollection.BuildServiceProvider());
+    }
+}
